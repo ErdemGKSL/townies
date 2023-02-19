37cc01e621dee45af9f2e3642f7076593e9d4b8c
ErdemGKSL <zahrakon06@gmail.com>
2023-02-19 23:57:50 +0300

    Townies

--- publish/bump.js
+++ publish/bump.js
@@ -19,10 +19,30 @@ const packageJsonPath = path.resolve(process.cwd(), './package.json');
 const packageJsonContent = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 
 // get the current version
-const currentVersion = packageJsonContent.version;
+
+
+// determine the type of bump
+const releaseType = isRelease ? 'patch' : 'prerelease';
+const publishType = isAlpha ? 'alpha' : isBeta ? 'beta' : null;
+
+let currentVersion;
+try { currentVersion = fs.readFileSync(path.resolve(process.cwd(), `./publish/version.txt`), "utf-8"); } catch { currentVersion = "0.0.0"; }
+try { if (publishType) currentVersion += "-" + fs.readFileSync(path.resolve(process.cwd(), `./publish/${publishType ?? "release"}.txt`), "utf-8"); } catch { if (publishType) currentVersion += `-${publishType}.0`; }
 
 // bump the version
-const newVersion = semver.inc(currentVersion, isRelease ? 'patch' : 'prerelease', isAlpha ? 'alpha' : isBeta ? 'beta' : 'rc');
+const newVersion = semver.inc(currentVersion, releaseType, publishType);
+console.log(currentVersion, releaseType, publishType)
+console.log(newVersion)
+// write the new version to the file
+fs.writeFileSync(path.resolve(process.cwd(), `./publish/version.txt`), newVersion.split('-')[0]);
+if (newVersion.split('-')[1]) fs.writeFileSync(path.resolve(process.cwd(), `./publish/${publishType}.txt`), newVersion.split('-')[1]);
+else {
+    ["alpha", "beta", "rc"].forEach(type => {
+        try {
+            fs.unlinkSync(path.resolve(process.cwd(), `./publish/${type}.txt`));
+        } catch {}
+    });
+}
 
 // update the version in the package.json file
 packageJsonContent.version = newVersion;
