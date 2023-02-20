2b26e8cad013d86614e20382f454a05f776bae78
ErdemGKSL <zahrakon06@gmail.com>
2023-02-20 18:35:25 +0300

    Townies

--- publish/bump.js
+++ publish/bump.js
@@ -8,9 +8,6 @@ const semver = require('semver');
 const { execSync } = require('child_process');
 
 const args = process.argv.slice(2);
-const isRelease = args.includes('release');
-const isAlpha = args.includes('alpha');
-const isBeta = args.includes('beta');
 const isDev = args.includes('dev');
 
 const packageJsonPath = path.resolve(process.cwd(), './package.json');
@@ -18,37 +15,16 @@ const packageJsonPath = path.resolve(process.cwd(), './package.json');
 // read the package.json file
 const packageJsonContent = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 
-// get the current version
+if (isDev) {
+    const version = (packageJsonContent.version.split('-')[0]) + "-dev." + Math.floor(Date.now() / (1000 * 60));
+    packageJsonContent.version = version;
+} else {
+    // get the version from the package.json file
+    const version = packageJsonContent.version.split("-")[0];
 
-
-// determine the type of bump
-const releaseType = isRelease ? 'patch' : 'prerelease';
-const publishType = isAlpha ? 'alpha' : isBeta ? 'beta' : isDev ? "dev" : null;
-
-let currentVersion;
-try { currentVersion = fs.readFileSync(path.resolve(process.cwd(), `./publish/version.txt`), "utf-8"); } catch { currentVersion = "0.0.0"; }
-try { if (publishType) currentVersion += "-" + fs.readFileSync(path.resolve(process.cwd(), `./publish/${publishType ?? "release"}.txt`), "utf-8"); } catch { if (publishType) currentVersion += `-${publishType}.0`; }
-
-// bump the version
-let newVersion = (packageJsonContent.version.includes("-") && !publishType) ? currentVersion : (
-    semver.inc((publishType ? currentVersion : currentVersion.split("-")[0]), releaseType, publishType ?? "null")
-);
-
-if (newVersion.includes("-null")) newVersion = newVersion.split("-")[0];
-
-// write the new version to the file
-fs.writeFileSync(path.resolve(process.cwd(), `./publish/version.txt`), newVersion.split('-')[0]);
-if (newVersion.split('-')[1]) fs.writeFileSync(path.resolve(process.cwd(), `./publish/${publishType}.txt`), newVersion.split('-')[1]);
-else {
-    ["alpha", "beta", "rc", "dev"].forEach(type => {
-        try {
-            fs.unlinkSync(path.resolve(process.cwd(), `./publish/${type}.txt`));
-        } catch {}
-    });
+    // bump the version
+    const newVersion = semver.inc(version, 'patch');;
+    packageJsonContent.version = newVersion;
 }
-
-// update the version in the package.json file
-packageJsonContent.version = newVersion;
-
 // write the package.json file
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJsonContent, null, 2));
