45074c13fd3f99dd5ca925a828022d479c6f4222
ErdemGKSL <zahrakon06@gmail.com>
2023-02-20 18:37:50 +0300

    Townies

--- publish/bump.js
+++ publish/bump.js
@@ -11,20 +11,15 @@ const args = process.argv.slice(2);
 const isDev = args.includes('dev');
 
 const packageJsonPath = path.resolve(process.cwd(), './package.json');
-
-// read the package.json file
 const packageJsonContent = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 
 if (isDev) {
     const version = (packageJsonContent.version.split('-')[0]) + "-dev." + Math.floor(Date.now() / (1000 * 60));
     packageJsonContent.version = version;
 } else {
-    // get the version from the package.json file
     const version = packageJsonContent.version.split("-")[0];
 
-    // bump the version
     const newVersion = semver.inc(version, 'patch');;
     packageJsonContent.version = newVersion;
 }
-// write the package.json file
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJsonContent, null, 2));
